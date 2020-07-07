f9dfa8169f90d126b27438159525c545c652e9ef
Kush Panjabi <kushpanjabi@kushs-mbp.lan>
2020-07-07 09:41:29 -0700

    completed UI build

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import React from 'react';
+import { Header } from './components/Header';
+import { Balance } from './components/Balance';
+import { IncExp } from './components/IncExp';
+import { TransactionList } from './components/TransactionList';
+import { AddTransaction } from './components/AddTransaction';
+
+
 import './App.css';
 
 function App() {
   return (
     <div>
-      Hello World!
+      <Header />
+      <div className="container">
+        <Balance />
+        <IncExp />
+        <TransactionList />
+        <AddTransaction />
+      </div>
     </div>
   );
 }
 
 export default App;
+ 
